8c7be0c50556a7dface628de44a3d53a4bf80b87
laike9m <laike9m@gmail.com>
2014-02-19 20:53:40 +0800

    basic server and client

---
+++ client.py
@@ -0,0 +1,46 @@
+import socket
+import sys
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+	print('Failed to create socket %s.' % msg)
+	sys.exit()
+	
+print('Socket Created')
+
+host = 'www.google.com.hk'
+port = 80
+ 
+try:
+    remote_ip = socket.gethostbyname(host)
+except socket.gaierror:
+    #could not resolve
+    print('Hostname could not be resolved. Exiting')
+    sys.exit()
+
+print('Ip address of ' + host + ' is ' + remote_ip)
+
+s.connect((remote_ip, port))
+
+print('Socket Connected to ' + host + ' on ip ' + remote_ip)
+
+# Send some data to remote server
+message = "GET / HTTP/1.1\r\n\r\n".encode('utf-8')
+ 
+try :
+    # Set the whole string
+    s.sendall(message)
+except socket.error:
+    # Send failed
+    print('Send failed')
+    sys.exit()
+ 
+print('Message send successfully')
+
+# Receive data
+reply = s.recv(4096).decode()
+print(reply)
+
+s.close()
+
---
+++ server.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+
+from threading import Thread
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8887 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+ 
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Bind failed %s.' % msg)
+    sys.exit()
+     
+print('Socket bind complete')
+
+s.listen(10)
+print('Socket now listening')
+
+# now keep talking with the client
+def client_thread(conn):
+	while True:
+		conn.send('Welcome to the server. Type something and hit enter\n'.encode('utf-8'))
+
+		# wait to accept a connection - blocking call
+		data = conn.recv(1024).decode('utf-8')
+		reply = 'OK...' + data
+		if not data:
+			break
+		 
+		conn.sendall(reply.encode('utf-8'))
+	conn.close()
+
+
+while True:
+    # wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	print('Connected with ' + addr[0] + ':' + str(addr[1]))
+     
+	# start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+	new_conn = Thread(target=client_thread ,args=(conn,))
+	new_conn.start()
+
+s.close()
