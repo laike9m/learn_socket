1ac59f939aa22e83d26b75b00e15e8b51b7495ea
laike9m <laike9m@gmail.com>
2014-03-09 00:10:27 +0800

    add output

--- punch/udp_punch_server.py
+++ punch/udp_punch_server.py
@@ -43,26 +43,27 @@ def main():
 
     poolqueue = {}
     while True:
-		data, addr = sockfd.recvfrom(32)
-		print "connection from %s:%d" % addr
+        data, addr = sockfd.recvfrom(32)
+        print "connection from %s:%d" % addr
 
-		pool = data.strip()
-		sockfd.sendto("ok "+pool, addr)
-		data, addr = sockfd.recvfrom(2)
-		if data != "ok":
-			continue
+        pool = data.strip()
+        sockfd.sendto("ok "+pool, addr)
+        print("pool={0}, ok sent to client".format(pool))
+        data, addr = sockfd.recvfrom(2)   # 卡在这里
+        if data != "ok":
+            continue
 
-		print "request received for pool:", pool
+        print "request received for pool:", pool
 
-		try:
-			a, b = poolqueue[pool], addr
-			sockfd.sendto(addr2bytes(a), b)
-			sockfd.sendto(addr2bytes(b), a)
-			print "linked", pool
-			del poolqueue[pool]
-		except KeyError:
-			poolqueue[pool] = addr
+        try:
+            a, b = poolqueue[pool], addr
+            sockfd.sendto(addr2bytes(a), b)
+            sockfd.sendto(addr2bytes(b), a)
+            print "linked", pool
+            del poolqueue[pool]
+        except KeyError:
+            poolqueue[pool] = addr
 
 
 if __name__ == "__main__":
-	main()
+    main()
